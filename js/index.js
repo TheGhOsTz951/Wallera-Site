5dba3d615092a75d995f8bf7391036e4d5899e2c
Sasso <ricciuti.alessio@hotmail.com>
2020-10-17 22:40:40 +0200

    The start

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/*function schedaClick() {
+    let schedaFronte = document.getElementById("scheda-fronte");
+    let schedaDietro = document.getElementById("scheda-dietro");
+
+    if (schedaFronte.style.display == "block" || schedaFronte.style.display == "") {
+        schedaFronte.style.display = "none";
+        schedaDietro.style.display = "block";
+    } else {
+        schedaFronte.style.display = "block";
+        schedaDietro.style.display = "none";
+    }
+}
+*/
